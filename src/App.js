1b67aaeb1bf25e80846f206b68433e3538583e5e
tamaki <hbk2002320@gmail.com>
2021-04-30 22:32:03 +0900

    usereducerとusecontextの組み合わせ

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import logo from "./logo.svg";
 import "./App.css";
 // import Basic2 from './components/Basic2'
@@ -7,12 +7,30 @@ import "./App.css";
 // import ApiFetch from "./components/ApiFetch";
 import AppContext from "./contexts/AppContext";
 // import B from './components/B'
-import BasicReducer from './components/BasicReducer';
+// import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB'
 
+
+const initialState = 0
+const reducer = (currentState, action) => {
+  switch(action) {
+    case 'add_1':
+      return currentState + 1
+    case 'multiple_3':
+      return currentState * 3
+    case 'reset':
+      return initialState
+    default:
+      return currentState
+  }
+}
 
 function App() {
+
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +40,11 @@ function App() {
           {/*<TimerContainer />*/}
           {/* <ApiFetch />
           <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          
+          <h2>Count {count}</h2>
+          <CompB />
+
         </header>
       </div>
     </AppContext.Provider>

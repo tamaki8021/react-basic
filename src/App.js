9aaec8921f24ac88eafe44d41a25a3b03827a00a
tamaki <hbk2002320@gmail.com>
2021-04-30 21:31:05 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from "./logo.svg";
+import "./App.css";
 // import Basic2 from './components/Basic2'
 // import BasicUseEffect from "./components/BasicUseEffect";
 // import TimerContainer from "./components/TimerContainer";
 import ApiFetch from "./components/ApiFetch";
+import AppContext from "./contexts/AppContext";
+import B from './components/B'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContext.Provider value={"value from App.js"}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
 
-        {/*<Basic2/>*/}
-        {/*<BasicUseEffect />*/}
-        {/*<TimerContainer />*/}
-        <ApiFetch/>
-
-      </header>
-    </div>
+          {/*<Basic2/>*/}
+          {/*<BasicUseEffect />*/}
+          {/*<TimerContainer />*/}
+          <ApiFetch />
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 

118dd869044905895e3674d944b07d12d92f321f
tamaki <hbk2002320@gmail.com>
2021-04-30 12:29:30 +0900

    add useEffect-axios/fetchを使ったAPIのデータ取得

---
+++ src/components/ApiFetch.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+const ApiFetch = () => {
+
+  const [posts, setPosts] = useState([])
+  const [id, setId] = useState(1)
+  const [clicked, setClicked] = useState(false)
+
+
+  const handlerClicked = () => {
+    setClicked(!clicked)
+  }
+
+  useEffect(() => {
+    // axiosを使ったデータ取得
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    //   .then(res => {
+    //     setPosts(res.data)
+    //   })
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {method: 'GET'})
+      .then(res => res.json())
+        .then(data => {
+          setPosts(data)
+        })
+  },[clicked])
+
+  return (
+    <div>
+      <input type='text' value={id} onChange={e => setId(e.target.value)} />
+      <br />
+      <button type='button' onClick={handlerClicked}>Get post</button>
+      <br />
+      {posts.title}
+
+      {/*<ul>*/}
+        {/*{posts.map(post => (*/}
+        {/*  <li key={post.id}>{post.title}</li>*/}
+        {/*))}*/}
+      {/*</ul>*/}
+    </div>
+  );
+};
+
+export default ApiFetch;
